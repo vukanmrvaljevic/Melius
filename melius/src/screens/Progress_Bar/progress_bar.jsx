e7e7f224eea94919a071005ca0d99b3e4ce34a18
vukanmrvaljevic <vukanmrvaljevic@gmail.com>
2021-01-25 17:52:10 -0500

    added an option for the user to create a new tab, and added a progress bar

--- melius/src/screens/Progress_Bar/progress_bar.jsx
+++ melius/src/screens/Progress_Bar/progress_bar.jsx
@@ -1,111 +1,45 @@
-import React, {Component} from "react"
-import './progress_bar.css'
-class progress_bar extends Component {
-    constructor() {
-      super();
-      
-      this._handleTabChange = this._handleTabChange.bind(this)
-    }
-    
-    _handleTabChange(index) {
-      console.log('Selected tab index', index);
-    }
-  
-    render() {
-      return (
-        <TabPanel onTabChange={this._handleTabChange}>
-          <div title="Tab 1">
-            <h1>Tab 1 progress content</h1>
-          </div>
-          <div title="Tab 2">
-            <h1>Tab 2 progress content</h1>
-          </div>
-          <div title="Tab 3">
-            <h1>Tab 3 progress content</h1>
-          </div>
-        </TabPanel>
-      );
-    }
-  }
-  
-  class TabPanel extends Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        selectedTabIndex: props.children.length === 0 ? null : 0
-      };
-  
-      this._handleClick = this._handleClick.bind(this);
-    }
-  
-    _handleClick(index) {
-      this.setState({
-        selectedTabIndex: index
-      });
-      
-      this.props.onTabChange(index);
-    }
-  
-    _renderTabs() {
-      let tabs = [];
-      
-      for (let i = 0; i < this.props.children.length; i++) {
-        tabs.push(
-          <Tab
-            key={`tab-${i}`}
-            label={this.props.children[i].props.title}
-            index={i}
-            isSelected={i === this.state.selectedTabIndex}
-            onClick={this._handleClick} />
-        );
-      }
-  
-      return tabs;
-    }
-    
-    _renderTabContent() {
-      return this.props.children[this.state.selectedTabIndex].props.children;
-    }
-  
-    render() {
-      if (this.state.selectedTabIndex == null) {
-        return 'nope';
-      }
-      
-      return (
-        <div className="tab-panel">
-          <div className="tab-panel__header">
-            {this._renderTabs()}
-          </div>
-          <div className="tab-panel__content">
-            {this._renderTabContent()}
-          </div>
-        </div>
-     );
-    }
-  }
-  
-  class Tab extends Component {
-    constructor(props) {
-      super(props);
-  
-      this._handleClick = this._handleClick.bind(this);
-    }
-  
-    _handleClick() {
-      this.props.onClick(this.props.index);
-    }
-  
-    render() {
-      const tabClassName = this.props.isSelected ? 'tab tab--selected' : 'tab';
-  
-      return (
-        <span className={tabClassName} onClick={this._handleClick}>
-          <span className="tab__label">{this.props.label}</span>
-        </span>
-      );
-    }
-  }
+import React from 'react';
+import './progress_bar.css';
 
-  export default progress_bar
+class progress_bar extends React.Component {
+    state = {
+      progress:  50    
+    };
+  
+    handleIncrement = () => {
+      if (this.state.progress !== 100) {
+            this.setState(prevState => ({progress: prevState.progress + 1}), 
+              () => console.log(this.state.progress));
+      }
+      else alert("oops, you hit the max value!")
+    }
+    
+    handleDecrement = () => {
+      if (this.state.progress !== 0) {
+           this.setState(prevState => ({progress: prevState.progress - 1}), 
+              () => console.log(this.state.progress));
+      }
+      else alert("oops, you hit the min value!")
+    }
+  
+    render() {
+      const test = this.state.progress + "%";
+      var style = { width: test }
+  
+      return (
+        <div className="flex-container">
+          <div className="progress" data-label={`${test} completed`}>
+            <span className="value" style={style}></span>
+          </div>
+          <br/>
+          <div>
+             <button className="button" onClick={this.handleDecrement}>-</button>
+            <button className="button" onClick={this.handleIncrement}>+</button>
+          </div>
+          
+       </div>
+      )
+    }
+} 
+  
+export default progress_bar
--- melius/src/screens/Table_Viewing/Table_Viewing.jsx
+++ melius/src/screens/Table_Viewing/Table_Viewing.jsx
@@ -1,113 +1,202 @@
-import React, {Component} from "react"
-import "./Table_Viewing.css"
+import React from "react";
+import uuid from "react-uuid";
 
-class table_viewing extends Component {
-        constructor() {
-          super();
-          
-          this._handleTabChange = this._handleTabChange.bind(this)
-        }
-        
-        _handleTabChange(index) {
-          console.log('Selected tab index', index);
-        }
-      
-        render() {
-          return (
-            <TabPanel onTabChange={this._handleTabChange}>
-              <div title="Tab 1">
-                <h1>Tab 1 table content</h1>
-              </div>
-              <div title="Tab 2">
-                <h1>Tab 2 table content</h1>
-              </div>
-              <div title="Tab 3">
-                <h1>Tab 3 table content</h1>
-              </div>
-            </TabPanel>
-          );
-        }
+import "./Table_Viewing.css";
+
+class Table_Viewing extends React.Component {
+  state = {
+    tabs: [
+          {id: 1, name: "Tab 1", content: "Wow this is tab 1"},
+    ],
+      currentTab: {
+          id: 1, name: "Tab 1", content: "Wow this is tab 1"},
+    editMode: false,
+    editTabNameMode: false
+  };
+
+  handleDoubleClick = () => {
+    this.setState({
+      editTabNameMode: true
+    });
+  };
+
+  handleEditTabName = e => {
+    const { currentTab, tabs } = this.state;
+
+    const updatedTabs = tabs.map(tab => {
+      if (tab.id === currentTab.id) {
+        return {
+          ...tab,
+          name: e.target.value
+        };
+      } else {
+        return tab;
       }
-      
-      class TabPanel extends Component {
-        constructor(props) {
-          super(props);
-      
-          this.state = {
-            selectedTabIndex: props.children.length === 0 ? null : 0
-          };
-      
-          this._handleClick = this._handleClick.bind(this);
-        }
-      
-        _handleClick(index) {
-          this.setState({
-            selectedTabIndex: index
-          });
-          
-          this.props.onTabChange(index);
-        }
-      
-        _renderTabs() {
-          let tabs = [];
-          
-          for (let i = 0; i < this.props.children.length; i++) {
-            tabs.push(
-              <Tab
-                key={`tab-${i}`}
-                label={this.props.children[i].props.title}
-                index={i}
-                isSelected={i === this.state.selectedTabIndex}
-                onClick={this._handleClick} />
-            );
-          }
-      
-          return tabs;
-        }
-        
-        _renderTabContent() {
-          return this.props.children[this.state.selectedTabIndex].props.children;
-        }
-      
-        render() {
-          if (this.state.selectedTabIndex == null) {
-            return 'nope';
-          }
-          
-          return (
-            <div className="tab-panel">
-              <div className="tab-panel__header">
-                {this._renderTabs()}
-              </div>
-              <div className="tab-panel__content">
-                {this._renderTabContent()}
-              </div>
-            </div>
-         );
-        }
+    });
+
+    this.setState({
+      tabs: updatedTabs,
+      currentTab: {
+        ...currentTab,
+        name: e.target.value
       }
-      
-      class Tab extends Component {
-        constructor(props) {
-          super(props);
-      
-          this._handleClick = this._handleClick.bind(this);
-        }
-      
-        _handleClick() {
-          this.props.onClick(this.props.index);
-        }
-      
-        render() {
-          const tabClassName = this.props.isSelected ? 'tab tab--selected' : 'tab';
-      
-          return (
-            <span className={tabClassName} onClick={this._handleClick}>
-              <span className="tab__label">{this.props.label}</span>
-            </span>
-          );
-        }
-      }
+    });
+  };
+
+  handleOnBlur = () => {
+    this.setState({
+      editTabNameMode: false
+    });
+  };
+
+  createTabs = () => {
+    const { tabs, currentTab, editTabNameMode } = this.state;
+
+    const allTabs = tabs.map(tab => {
+      return (
+        <li>
+          {editTabNameMode && currentTab.id === tab.id ? (
+            <input
+              value={tab.name}
+              onBlur={this.handleOnBlur}
+              onChange={this.handleEditTabName}
+            />
+          ) : (
+            <button
+              className={currentTab.id === tab.id ? "tab active" : "tab"}
+              onClick={() => this.handleSelectTab(tab)}
+              onDoubleClick={() => this.handleDoubleClick(tab)}
+            >
+              {tab.name}
+            </button>
+          )}
+        </li>
+      );
+    });
+
+    return <ul className="nav nav-tabs">{allTabs}</ul>;
+  };
+
+  handleSelectTab = tab => {
+    this.setState({
+      currentTab: tab,
+      editMode: false,
+      editTabNameMode: false
+    });
+  };
+
+  handleAddTab = () => {
+    const { tabs } = this.state;
+
+    const newTabObject = {
+      id: uuid(),
+      name: `Tab ${tabs.length + 1}`,
+      content: `This is Tab ${tabs.length + 1}`
+    };
     
 
-export default table_viewing
+    this.setState({
+      tabs: [...tabs, newTabObject],
+      currentTab: newTabObject,
+      editMode: false,
+      editTabNameMode: false
+    });
+  };
+
+  handleDeleteTab = tabToDelete => {
+    const { tabs } = this.state;
+    const tabToDeleteIndex = tabs.findIndex(tab => tab.id === tabToDelete.id);
+
+    const updatedTabs = tabs.filter((tab, index) => {
+      return index !== tabToDeleteIndex;
+    });
+
+    const previousTab =
+      tabs[tabToDeleteIndex - 1] || tabs[tabToDeleteIndex + 1] || {};
+
+    this.setState({
+      tabs: updatedTabs,
+      editMode: false,
+      editTabNameMode: false,
+      currentTab: previousTab
+    });
+  };
+
+  setEditMode = () => {
+    this.setState({
+      editMode: !this.state.editMode
+    });
+  };
+
+  handleContentChange = e => {
+    const { tabs, currentTab } = this.state;
+
+    const updatedTabs = tabs.map(tab => {
+      if (tab.name === currentTab.name) {
+        return {
+          ...tab,
+          content: e.target.value
+        };
+      } else {
+        return tab;
+      }
+    });
+
+    this.setState({
+      tabs: updatedTabs,
+      currentTab: {
+        ...currentTab,
+        content: e.target.value
+      }
+    });
+  };
+
+  render() {
+    const { currentTab, editMode } = this.state;
+    return (
+      <div className="container">
+        <div className="well">
+          <button className="add-tab-button" onClick={this.handleAddTab}>
+            <i className="text-primary fas fa-plus-square" /> Add Tab
+          </button>
+          {this.createTabs()}
+          <div className="tab-content">
+            {editMode ? (
+              <div>
+                <textarea
+                  onChange={this.handleContentChange}
+                  value={this.state.currentTab.content}/>
+                <button className="save-button" onClick={this.setEditMode}>
+                  Done
+                </button>
+              </div>
+            ) : (
+              <div>
+                <p>{currentTab.content}</p>
+                {currentTab.id ? (
+                  <div
+                    style={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <button
+                      className="edit-mode-button"
+                      onClick={this.setEditMode}
+                    >
+                      Edit
+                    </button>
+                    <button onClick={() => this.handleDeleteTab(currentTab)}>
+                      Delete
+                    </button>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Table_Viewing
