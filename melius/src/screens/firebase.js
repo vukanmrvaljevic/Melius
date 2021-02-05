6ee91b87f0b321f3fa6265470c8aee7f15f5d326
forexsnyder <forexsnyder@gmail.com>
2021-02-05 14:13:21 -0800

    Auth

--- melius/src/screens/firebase.js
+++ melius/src/screens/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyD_6yNxi41oYMHk__0uLu5riEjdQRQjmEM",
-  authDomain: "melius-development.firebaseapp.com",
-  databaseURL: "https://melius-development-default-rtdb.firebaseio.com",
-  projectId: "melius-development",
-  storageBucket: "melius-development.appspot.com",
-  messagingSenderId: "1059298642106",
-  appId: "1:1059298642106:web:8d2295467e174334e51a83",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
 export const auth = firebase.auth()
